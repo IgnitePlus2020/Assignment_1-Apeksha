88d9f634d60a56b6301167cffffff8f1bd42f0d6
Apeksha245 <apekshajadhav65@gmail.com>
2020-03-15 19:45:26 +0530

    Start

---
+++ src/com/tgt/igniteplus/pyramid.java
@@ -0,0 +1,19 @@
+package com.tgt.igniteplus;
+import java.util.*;
+public class pyramid
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the height of the pyramid");
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
